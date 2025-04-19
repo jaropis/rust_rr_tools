1c7a8f594d63b51ba57549a73ec0eda594068b4f
Jaroslaw Piskorski <jaropis@zg.home.pl>
2025-04-19 20:00:36 +0200

    diffing correctly

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ use std::path::PathBuf;
 #[derive(Debug)]
 struct Arguments<'a> {
     input_extension: &'a String,
+    rr_multiplier: f32,
 }
 
 fn parse_args(argv: &Vec<String>) -> Arguments {
     Arguments {
         input_extension: &argv[1],
+        rr_multiplier: 1000.0,
     }
 }
 
-fn read_lines(filepath: &str) -> Vec<Vec<String>> {
+fn read_lines(filepath: &str, args: &Arguments) -> Vec<Vec<String>> {
     let contents = match fs::read_to_string(filepath) {
         Ok(data) => data,
         Err(_) => {
@@ -22,13 +24,40 @@ fn read_lines(filepath: &str) -> Vec<Vec<String>> {
     };
 
     let owned_lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+    let diff = true;
     let mut result: Vec<Vec<String>> = Vec::new();
+    let mut rr_idx: i32 = 0;
+    let mut prev: f32 = 0.0;
+    let mut current: f32;
     for s in &owned_lines {
         let mut line: Vec<String> = Vec::new();
-        for word in s.split_whitespace() {
-            line.push(word.to_string());
+        for (i, word) in s.split_whitespace().enumerate() {
+            let mut owned_word = word.to_string();
+            if i == 0 {
+                if let Ok(num) = owned_word.parse::<f32>() {
+                    if diff {
+                        current = num - prev;
+                        println!("current: {}, prev: {}, num: {}", current, prev, num);
+                        prev = num;
+                    } else {
+                        current = num
+                    }
+
+                    if diff && rr_idx == 0 {
+                        prev = num;
+                    }
+                    println!("current: {}, prev: {}, num: {}", current, prev, num);
+                    owned_word = (current * args.rr_multiplier).to_string();
+                }
+            }
+            if (diff && rr_idx > 0) || (!diff) {
+                line.push(owned_word);
+            }
         }
-        result.push(line);
+        if (diff && rr_idx > 0) || (!diff) {
+            result.push(line);
+        }
+        rr_idx += 1;
     }
 
     result
@@ -44,7 +73,7 @@ fn main() -> io::Result<()> {
         let entry_path = entry.unwrap().path();
         if entry_path.extension().and_then(|s| s.to_str()) == Some(args.input_extension) {
             println!("file: {:?}", entry_path);
-            let contents = read_lines(&entry_path.to_str().unwrap());
+            let contents = read_lines(&entry_path.to_str().unwrap(), &args);
             println!("contents length: {:?}", contents);
         }
     }
