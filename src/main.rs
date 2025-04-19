259561cf7bea8a14d4a9e68783fe3064c9d408c5
Jaroslaw Piskorski <jaropis@zg.home.pl>
2025-04-19 12:31:55 +0200

    reading lines into vector

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,26 @@ fn parse_args(argv: &Vec<String>) -> Arguments {
     }
 }
 
-fn read_lines(filepath: &str) -> String {
-    match fs::read_to_string(filepath) {
+fn read_lines(filepath: &str) -> Vec<Vec<String>> {
+    let contents = match fs::read_to_string(filepath) {
         Ok(data) => data,
         Err(_) => {
             println!("failed to read from file {}", &filepath);
             std::process::exit(1);
         }
+    };
+
+    let owned_lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+    let mut result: Vec<Vec<String>> = Vec::new();
+    for s in &owned_lines {
+        let mut line: Vec<String> = Vec::new();
+        for word in s.split_whitespace() {
+            line.push(word.to_string());
+        }
+        result.push(line);
     }
+
+    result
 }
 
 fn main() -> io::Result<()> {
@@ -33,7 +45,7 @@ fn main() -> io::Result<()> {
         if entry_path.extension().and_then(|s| s.to_str()) == Some(args.input_extension) {
             println!("file: {:?}", entry_path);
             let contents = read_lines(&entry_path.to_str().unwrap());
-            println!("contents: {}", contents);
+            println!("contents length: {:?}", contents);
         }
     }
     Ok(())
